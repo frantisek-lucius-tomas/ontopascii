0b4aa6886ca6443a35bbeacb57db9e0d3c9c0319
frantisektomas <wfrantisektomas@gmail.com>
2024-08-07 15:14:55 +0200

    better gray scale

--- image-to-ascii.py
+++ image-to-ascii.py
@@ -1,46 +1,53 @@
-import math
+import math, numpy as np
 from PIL import Image, ImageDraw, ImageFont
 
-chars = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^'. "[::-1]
-charArray = list(chars)
+# characters for the ASCII art
+chars = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`'. "[::-1]
+charArray = np.array(list(chars))
 charLength = len(charArray)
 interval = charLength / 256
 
-# scale this number for better rendering
-scaleFactor = 0.2
+scaleFactor = 0.25
 
 oneCharWidth = 10
-oneCharHeight = 18
+oneCharHeight = 14
 
 def getChar(inputInt):
     return charArray[math.floor(inputInt * interval)]
 
-text_file = open("output.txt", "w")
+# open the image
+im = Image.open("images/chameleon.jpg").convert('RGB')
 
-# place here your image here that you want in ascii
-im = Image.open("here.png").convert('RGB')
-
+# use a font available on your system
 fnt = ImageFont.truetype('C:\\Windows\\Fonts\\lucon.ttf', 15)
 
 width, height = im.size
+# resize image based on the scale factor and character dimensions
 im = im.resize((int(scaleFactor * width), int(scaleFactor * height * (oneCharWidth / oneCharHeight))), Image.NEAREST)
 width, height = im.size
-pix = im.load()
+pix = np.array(im)
 
+# create an output image
 outputImage = Image.new('RGB', (oneCharWidth * width, oneCharHeight * height), color=(0, 0, 0))
 d = ImageDraw.Draw(outputImage)
 
-for i in range(height):
-    for j in range(width):
-        r, g, b = pix[j, i]
-        h = int(r/3 + g/3 + b/3)
-        h = min(255, h)
-        pix[j, i] = (h, h, h)
-        text_file.write(getChar(h))
-        d.text((j * oneCharWidth, i * oneCharHeight), getChar(h), font=fnt, fill=(r, g, b))
+# create an output text file
+with open("output.txt", "w") as text_file:
+    for i in range(height):
+        for j in range(width):
+            r, g, b = pix[i, j]
+            # convert the RGB value to grayscale using the luminosity formula
+            gray = int(0.299*r + 0.587*g + 0.114*b)
+            gray = min(255, gray)
+            char = getChar(gray)
+            
+            # write character to text file
+            text_file.write(char)
+            
+            # draw character on the output image
+            d.text((j * oneCharWidth, i * oneCharHeight), char, font=fnt, fill=(r, g, b))
+        
+        text_file.write('\n')
 
-    text_file.write('\n')
-
-text_file.close()
-
-outputImage.save('output.png')
+# save the output image
+outputImage.save('output/output.png')
