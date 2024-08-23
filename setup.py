ab2a1a82e82f9e18ca7f3754ca0d5820f1651b4c
frantisektomas <wfrantisektomas@gmail.com>
2024-08-23 22:09:30 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="asciiart",
-    version="0.1.0",
+    version="0.0.1",
     description="A Python library to convert images and GIFs into ASCII art.",
     author="frantisek tomas",
     author_email="wfrantisektomas@gmail.com",
