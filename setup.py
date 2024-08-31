a14e972fa56b9d5d2fba79c7d356832cbf589188
frantisektomas <wfrantisektomas@gmail.com>
2024-08-31 09:17:35 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name="ontopascii",
-    version="0.0.5",
+    version="0.0.7",
     author="frantisek tomas",
     author_email="wfrantisektomas@gmail.com",
     url="https://github.com/frantisek-lucius-tomas/ontopascii",
-    description="A library for generating ASCII art easily and effectively.",
-    long_description=open("README.md").read(),
+    description="more on github",
     long_description_content_type="text/markdown",
     packages=find_packages(),
     install_requires=[
